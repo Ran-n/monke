aead58aef78a4b004cce8194667e94cad02216e7
Ran# <ran-n@tutanota.com>
2022-10-02 22:44:05 +0200

    Added email to description

--- src/no_b4_u_continue_yt.js
+++ src/no_b4_u_continue_yt.js
@@ -4,7 +4,7 @@
 // @match       https://www.youtube.com/
 // @grant       none
 // @version     1.0
-// @author      Ran#
+// @author      Ran# <ran-n@tutanota.com>
 // @description 10/2/2022, 10:25:22 PM
 // @homepageURL https://github.com/Ran-n/monke/blob/main/src/no_b4_u_continue_yt.js
 // @downloadURL https://raw.githubusercontent.com/Ran-n/monke/main/src/no_b4_u_continue_yt.js
