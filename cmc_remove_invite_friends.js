5d6f34560cb7cfbe91aed54684b392576cb078e1
Ran# <ran-n@tutanota.com>
2022-10-02 22:36:02 +0200

    added urls

--- cmc_remove_invite_friends.js
+++ cmc_remove_invite_friends.js
@@ -6,6 +6,8 @@
 // @version     1.0
 // @author      Ran#
 // @description 10/2/2022, 10:17:36 PM
+// @downloadURL https://raw.githubusercontent.com/Ran-n/monke/main/cmc_remove_invite_friends.js
+// @homepageURL https://github.com/Ran-n/monke/blob/main/cmc_remove_invite_friends.js
 // ==/UserScript==
 
 elto=document.getElementsByClassName("sc-1snuar3-11 cIdfQu")[0];
