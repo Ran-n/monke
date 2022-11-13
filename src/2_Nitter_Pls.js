2c8958971f88bed56f1669be28598c29ce0acdad
Ran# <ran-n@tutanota.com>
2022-11-13 23:29:15 +0100

    updated metadata of 2_Nitter_Pls.js

--- src/2_Nitter_Pls.js
+++ src/2_Nitter_Pls.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        2NitterPls
+// @name        2 Nitter Pls
 // @namespace   Violentmonkey Scripts
 // @match       https://twitter.com/*
 // @match       https://nitter.*/*
 // @grant       none
 // @version     1.0
-// @author      -
+// @author      Ran# <ran-n@tutanota.com>
 // @description 11/9/2022, 11:26:12 AM
 // @homepageURL https://github.com/Ran-n/monke/blob/main/src/2_Nitter_Pls.js
 // @downloadURL https://raw.githubusercontent.com/Ran-n/monke/main/src/2_Nitter_Pls.js
