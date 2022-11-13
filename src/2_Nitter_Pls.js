250668126bdbf13659b116f2623d6f2e22217b0c
Ran# <ran-n@tutanota.com>
2022-11-14 00:01:22 +0100

    created 2_Bibliogram_Pls.js

--- src/2_Nitter_Pls.js
+++ src/2_Nitter_Pls.js
@@ -4,12 +4,11 @@
 // @match       https://twitter.com/*
 // @match       https://nitter.*/*
 // @grant       none
-// @version     1.1
+// @version     1.1.1
 // @author      Ran# <ran-n@tutanota.com>
 // @description 11/9/2022, 11:26:12 AM
 // @homepageURL https://github.com/Ran-n/monke/blob/main/src/2_Nitter_Pls.js
 // @downloadURL https://raw.githubusercontent.com/Ran-n/monke/main/src/2_Nitter_Pls.js
-// @updateURL   https://raw.githubusercontent.com/Ran-n/monke/main/src/2_Nitter_Pls.js
 // ==/UserScript==
 
 if (window.location.href.includes('twitter.com')) {
