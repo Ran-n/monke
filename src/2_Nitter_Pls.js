1542bc8e33d216bd51ba29fe22d89f3c49031275
Ran# <ran-n@tutanota.com>
2022-11-13 23:52:10 +0100

    update

--- src/2_Nitter_Pls.js
+++ src/2_Nitter_Pls.js
@@ -4,7 +4,7 @@
 // @match       https://twitter.com/*
 // @match       https://nitter.*/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Ran# <ran-n@tutanota.com>
 // @description 11/9/2022, 11:26:12 AM
 // @homepageURL https://github.com/Ran-n/monke/blob/main/src/2_Nitter_Pls.js
@@ -15,5 +15,3 @@
 if (window.location.href.includes('twitter.com')) {
     window.location.replace(window.location.href.replace('twitter.com', 'nitter.net'));
 }
-
-console.log('test')
