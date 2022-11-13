37c3eb3f2e946e190532b9596e28ad4e688e42c3
Ran# <ran-n@tutanota.com>
2022-11-13 23:41:20 +0100

    update url

--- src/2_Nitter_Pls.js
+++ src/2_Nitter_Pls.js
@@ -9,6 +9,7 @@
 // @description 11/9/2022, 11:26:12 AM
 // @homepageURL https://github.com/Ran-n/monke/blob/main/src/2_Nitter_Pls.js
 // @downloadURL https://raw.githubusercontent.com/Ran-n/monke/main/src/2_Nitter_Pls.js
+// @updateURL   https://raw.githubusercontent.com/Ran-n/monke/main/src/2_Nitter_Pls.js
 // ==/UserScript==
 
 if (window.location.href.includes('twitter.com')) {
