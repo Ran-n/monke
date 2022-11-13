7126e638ed7785eec9d85ff27b7ffa3ec0ba2c40
Ran# <ran-n@tutanota.com>
2022-11-14 00:26:41 +0100

    so that it ignores once the change is made

--- src/2_ProxiTok_Pls.js
+++ src/2_ProxiTok_Pls.js
@@ -5,12 +5,14 @@
 // @match       https://www.tiktok.com/*
 // @match       https://proxitok.pabloferreiro.es/*
 // @grant       none
-// @version     1.1
+// @version     1.1.1
 // @author      Ran# <ran-n@tutanota.com>
 // @description 14/9/2022, 00:06:38 AM
 // @homepageURL https://github.com/Ran-n/monke/blob/main/src/2_ProxiTok_Pls.js
 // @downloadURL https://raw.githubusercontent.com/Ran-n/monke/main/src/2_ProxiTok_Pls.js
 // ==/UserScript==
 
-var cachos_lig = window.location.href.split('?')[0].split('/')
-window.location.href = `https://proxitok.pabloferreiro.es/@placeholder/video/${cachos_lig[cachos_lig.length - 1]}`
+if (window.location.href.includes('tiktok')) {
+    var cachos_lig = window.location.href.split('?')[0].split('/')
+    window.location.href = `https://proxitok.pabloferreiro.es/@placeholder/video/${cachos_lig[cachos_lig.length - 1]}`
+}
