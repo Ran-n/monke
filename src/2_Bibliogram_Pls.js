07532415b081e9d32993f8078c049706192dd649
Ran# <ran-n@tutanota.com>
2022-11-14 00:09:11 +0100

    2_ProxiTok_Pls & update of 2_Bibliogram_Pls

--- src/2_Bibliogram_Pls.js
+++ src/2_Bibliogram_Pls.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name        2 Bibliogram Pls
 // @namespace   Violentmonkey Scripts
+// @match       https://www.instagram.com/*
 // @match       https://instagram.com/*
 // @match       https://bibliogram.org/*
 // @grant       none
-// @version     1.0
+// @version     1.0.2
 // @author      Ran# <ran-n@tutanota.com>
 // @description 13/9/2022, 23:59:50 AM
 // @homepageURL https://github.com/Ran-n/monke/blob/main/src/2_Bibliogram_Pls.js
 // @downloadURL https://raw.githubusercontent.com/Ran-n/monke/main/src/2_Bibliogram_Pls.js
 // ==/UserScript==
 
-if (window.location.href.includes('instagram.com')) {
+if (window.location.href.includes('www.instagram.com')) {
+    window.location.replace(window.location.href.replace('www.instagram.com', 'bibliogram.org'));
+} else if (window.location.href.includes('instagram.com')) {
     window.location.replace(window.location.href.replace('instagram.com', 'bibliogram.org'));
 }
